ac47d838c85b525332f88946347c19defc19b1eb
dylanbeattie <dylan@dylanbeattie.net>
2018-03-15 13:24:40 +0100

    Implements newName property on Profile entity

--- src/Herobook/Data/Entities/Profile.cs
+++ src/Herobook/Data/Entities/Profile.cs
@@ -1,16 +1,42 @@
 ﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
 
-namespace Herobook.Data.Entities {
-    public class Profile {
-        public Profile() { }
+namespace Herobook.Data.Entities
+{
+	public class Profile
+	{
+		public Profile() { }
 
-        public Profile(string username, string name) {
-            Username = username;
-            Name = name;
-        }
+		public Profile(string username, string name)
+		{
+			Username = username;
+			Name = name;
+		}
 
-        public string Name { get; set; }
-        public string Username { get; set; }
-        public DateTime? Birthday { get; set; }
-    }
+		[JsonIgnore]
+		public Name NewName {
+			get {
+				var tokens = this.Name.Split(' ');
+
+				return new Entities.Name
+				{
+					FirstName = tokens.Length > 1 ? tokens.First() : null,
+					LastName = tokens.LastOrDefault()
+				};
+			}
+		}
+
+		public string Name { get; set; }
+		public string Username { get; set; }
+		public DateTime? Birthday { get; set; }
+
+
+	}
+
+	public class Name
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
 }
